215fb08eb5b3669677c89522106d946b7a0e5328
ji-xuepeng <ji.xuepeng@zte.com.cn>
2017-02-06 21:27:14 +0800

    Use https instead of http for git.openstack.org
    Trivialfix
    
    Change-Id: I5b595713eeed1ea59756485866acb43d94aa7c19

--- releasenotes/source/conf.py
+++ releasenotes/source/conf.py
@@ -65,7 +65,7 @@ target_name = 'openstack-ansible-' + role_name
 title = 'OpenStack-Ansible Release Notes: ' + role_name + 'role'
 
 # The link to the browsable source code (for the left hand menu)
-oslosphinx_cgit_link = 'http://git.openstack.org/cgit/openstack/' + target_name
+oslosphinx_cgit_link = 'https://git.openstack.org/cgit/openstack/' + target_name
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the

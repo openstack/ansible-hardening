be148a0bad257d8ca2a826a18344ee433a8ca0af
pengyuesheng <pengyuesheng@gohighsec.com>
2019-07-31 17:10:11 +0800

    Bump the openstackdocstheme extension to 1.20
    Some options are now automatically configured by the version 1.20:
    - project
    - html_last_updated_fmt
    - latex_engine
    - latex_elements
    - version
    - release.
    
    Change-Id: I14b62b4010950877d58a615de5f671ab6c866b48

--- releasenotes/source/conf.py
+++ releasenotes/source/conf.py
@@ -123,7 +123,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+html_theme = 'openstackdocs'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -158,10 +158,6 @@ html_static_path = ['_static']
 # .htaccess) here, relative to this directory. These files are copied
 # directly to the root of the documentation.
 # html_extra_path = []
-
-# If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
-# using the given strftime format.
-html_last_updated_fmt = '%Y-%m-%d %H:%M'
 
 # If true, SmartyPants will be used to convert quotes and dashes to
 # typographically correct entities.
@@ -205,17 +201,6 @@ htmlhelp_basename = target_name + '-docs'
 
 
 # -- Options for LaTeX output ---------------------------------------------
-
-latex_elements = {
-    # The paper size ('letterpaper' or 'a4paper').
-    # 'papersize': 'letterpaper',
-
-    # The font size ('10pt', '11pt' or '12pt').
-    # 'pointsize': '10pt',
-
-    # Additional stuff for the LaTeX preamble.
-    # 'preamble': '',
-}
 
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title,
